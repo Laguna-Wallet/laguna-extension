ec28c093e29344a3b71ec379b7fe06fc32cc6211
Evelyn <evelyn@lagu.na>
2022-10-03 14:39:19 +0800

    update alchemyApi.getHistoricalTransactions

--- src/popup/src/utils/evm/api.ts
+++ src/popup/src/utils/evm/api.ts
@@ -3,6 +3,7 @@ import { IEVMAssetERC20, IEVMAsset, IEVMBuildTransaction, IEVMToBeSignTransactio
 import fs from "fs";
 import BigNumber from "bignumber.js";
 import { EVMNetwork, nativeCurrencyByNetwork, networks, assetByNetwork, EVMAssetId, EVMAssetType, assets } from "networks/evm";
+import * as alchemyApi from "./api/alchemy";
 
 export const toCheckSumAddress = (address: string): string => {
   const checksumAddress = ethers.utils.getAddress(address); 
@@ -167,104 +168,15 @@ export const getAssetIdBySmartContractAddress = (smartContractAddress: string, n
     return contractObject.symbol;
 };
 
-export const getHistoricalTransactions = async (address: string, network: EVMNetwork, assetId: EVMAssetId)
+export const getHistoricalTransactions = async (address: string, networkId: EVMNetwork, assetId: EVMAssetId)
 : Promise<IEVMHistoricalTransaction[]> => {
-  return getHistoricalTransactionsByAlchemy(address, network, assetId);
-};
-
-export const getHistoricalTransactionsByAlchemy = async (address: string, networkId: EVMNetwork, assetId: EVMAssetId, fromBlock?: BigNumber)
-: Promise<IEVMHistoricalTransaction[]> => {
-  const startTime = Date.now();
-  try {
-    // let pageKey: string | null = null;
-    const historicalTransactions: IEVMHistoricalTransaction[] = [];
-    const asset: IEVMAsset | IEVMAssetERC20 = assets[assetId];
-    let category: string[];
-    let contractAddresses: string[];
-    switch (assets[assetId].assetType) {
-      case EVMAssetType.NATIVE: {
-        category = ["internal", "external"];
-        contractAddresses = [];
-        break;
-      }
-      case EVMAssetType.ERC20: {
-        category = ["erc20"];
-        contractAddresses = [(asset as IEVMAssetERC20).contractAddress];
-        break;
-      }
-      default: 
-        category = [];
-        contractAddresses = [];
-    }
-    let param: IAlchemyTransferParam = {
-      fromBlock: fromBlock?.isFinite()? `0x${fromBlock.toString(16)}` : "0x0",
-      toBlock: "latest",
-      category: category,
-      withMetadata: false,
-      excludeZeroValue: false,
-      fromAddress: address,
-    };
-    if (asset.assetType !== EVMAssetType.NATIVE) {
-      param = {
-        ...param,
-        contractAddresses,
-      };
-    }
-    const options = {
-      method: "POST",
-      headers: {Accept: "application/json", "Content-Type": "application/json"},
-      body: JSON.stringify({
-        id: 1,
-        jsonrpc: "2.0",
-        method: "alchemy_getAssetTransfers",
-        params: [param],
-      }),
-    };
-  
-    const provider = getProvider(networkId);
-    const res = await fetch(networks[networkId].nodeUrl, options);
-    const data = await res.json();
-    // TODO remove log
-    console.log("*** res", res);
-    console.log("*** data", data);
-    if (data.error) {
-      throw data.error;
-    }
-    do {
-      for (const transfer of data.result.transfers) {
-          const transferObj: IEVMHistoricalTransaction  = {
-            assetId: assetId.toString(),
-            amount: new BigNumber(transfer.value),
-            from: toCheckSumAddress(transfer.from),
-            to: toCheckSumAddress(transfer.to),  
-            fee: new BigNumber(0),
-            nonce: new BigNumber(0),
-            blockNumber: new BigNumber(transfer.blockNumber),
-            transactionHash: transfer.hash,
-            timestamp: 0,
-          };
-          historicalTransactions.push(transferObj);
-      }
-    } while (data.result.pageKey);
-    // TODO remove log
-    console.log("TimeTaken 1 - ", Date.now() - startTime);
-    const chunkSize = 20;
-    for(let i = 0; i < historicalTransactions.length; i += chunkSize) {
-      const chunk = historicalTransactions.slice(i, i + chunkSize);
-      await Promise.all(
-        chunk.map( async (historicalTransaction) => {
-          const transaction =  await provider.getTransaction(historicalTransaction.transactionHash);
-          // const transactionReceipt = await provider.getTransactionReceipt(historicalTransaction.transactionHash);
-          // historicalTransaction.fee = new BigNumber(transactionReceipt.gasUsed.mul(transactionReceipt.effectiveGasPrice)._hex);
-          // historicalTransaction.nonce = new BigNumber(transaction.nonce);
-          historicalTransaction.timestamp = transaction.timestamp? transaction.timestamp : 0;
-        }));
-    }
-    // TODO remove log
-    console.log("TimeTaken 2 - ", Date.now() - startTime);
-    return historicalTransactions;
-  } catch(err) {
-    console.log("Failed to getHistoricalTransactionsByAlchemy", err);
-    throw err;
+  switch (networkId) {
+    case EVMNetwork.LOCALHOST:
+      return [];
+    case EVMNetwork.ETHEREUM:
+    case EVMNetwork.ETHEREUM_TESTNET_GOERLI:
+      return await alchemyApi.getHistoricalTransactions(address, networkId, assetId);
+    default:
+      throw new Error("Non Supported Network");
   }
 };
---
+++ src/popup/src/utils/evm/api/alchemy.ts
@@ -0,0 +1,109 @@
+import { IEVMAssetERC20, IEVMAsset, IEVMHistoricalTransaction, IAlchemyTransferParam } from "../interfaces";
+import BigNumber from "bignumber.js";
+import { EVMNetwork, EVMAssetId, EVMAssetType, assets } from "networks/evm";
+import { getProvider, toCheckSumAddress } from "../api";
+
+const getApiUrl = (networkId: EVMNetwork) : string => {
+    switch (networkId) {
+      case EVMNetwork.ETHEREUM:
+        return `https://eth-mainnet.g.alchemy.com/v2/${process.env.REACT_APP_ALCHEMY_KEY}`;
+      case EVMNetwork.ETHEREUM_TESTNET_GOERLI:
+        return `https://eth-mainnet.g.alchemy.com/v2/${process.env.REACT_APP_ALCHEMY_KEY}`;
+      default:
+        throw new Error("Non Supported Network");
+    }
+  };
+  
+  export const getHistoricalTransactions = async (address: string, networkId: EVMNetwork, assetId: EVMAssetId, fromBlock?: BigNumber)
+  : Promise<IEVMHistoricalTransaction[]> => {
+    const startTime = Date.now();
+    try {
+      const historicalTransactions: IEVMHistoricalTransaction[] = [];
+      const asset: IEVMAsset | IEVMAssetERC20 = assets[assetId];
+      let category: string[];
+      let contractAddresses: string[];
+      switch (assets[assetId].assetType) {
+        case EVMAssetType.NATIVE: {
+          category = ["internal", "external"];
+          contractAddresses = [];
+          break;
+        }
+        case EVMAssetType.ERC20: {
+          category = ["erc20"];
+          contractAddresses = [(asset as IEVMAssetERC20).contractAddress];
+          break;
+        }
+        default: 
+          category = [];
+          contractAddresses = [];
+      }
+      let param: IAlchemyTransferParam = {
+        fromBlock: fromBlock?.isFinite()? `0x${fromBlock.toString(16)}` : "0x0",
+        toBlock: "latest",
+        category: category,
+        withMetadata: false,
+        excludeZeroValue: false,
+        fromAddress: address,
+      };
+      if (asset.assetType !== EVMAssetType.NATIVE) {
+        param = {
+          ...param,
+          contractAddresses,
+        };
+      }
+      const options = {
+        method: "POST",
+        headers: {Accept: "application/json", "Content-Type": "application/json"},
+        body: JSON.stringify({
+          id: 1,
+          jsonrpc: "2.0",
+          method: "alchemy_getAssetTransfers",
+          params: [param],
+        }),
+      };
+    
+      const provider = getProvider(networkId);
+      const res = await fetch(getApiUrl(networkId), options);
+      const data = await res.json();
+      if (data.error) {
+        throw data.error;
+      }
+      do {
+        for (const transfer of data.result.transfers) {
+            const transferObj: IEVMHistoricalTransaction  = {
+              assetId: assetId.toString(),
+              amount: new BigNumber(transfer.value),
+              from: toCheckSumAddress(transfer.from),
+              to: toCheckSumAddress(transfer.to),  
+              fee: new BigNumber(0),
+              nonce: new BigNumber(0),
+              blockNumber: new BigNumber(transfer.blockNumber),
+              transactionHash: transfer.hash,
+              timestamp: 0,
+            };
+            historicalTransactions.push(transferObj);
+        }
+      } while (data.result.pageKey);
+      // TODO remove log
+      console.log("TimeTaken 1 - ", Date.now() - startTime);
+      const chunkSize = 20;
+      for(let i = 0; i < historicalTransactions.length; i += chunkSize) {
+        const chunk = historicalTransactions.slice(i, i + chunkSize);
+        await Promise.all(
+          chunk.map( async (historicalTransaction) => {
+            const transaction =  await provider.getTransaction(historicalTransaction.transactionHash);
+            // const transactionReceipt = await provider.getTransactionReceipt(historicalTransaction.transactionHash);
+            // historicalTransaction.fee = new BigNumber(transactionReceipt.gasUsed.mul(transactionReceipt.effectiveGasPrice)._hex);
+            // historicalTransaction.nonce = new BigNumber(transaction.nonce);
+            historicalTransaction.timestamp = transaction.timestamp? transaction.timestamp : 0;
+          }));
+      }
+      // TODO remove log
+      console.log("TimeTaken 2 - ", Date.now() - startTime);
+      return historicalTransactions;
+    } catch(err) {
+      console.log("Failed to getHistoricalTransactionsByAlchemy", err);
+      throw err;
+    }
+  };
+  
